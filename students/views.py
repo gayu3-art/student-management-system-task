1d0cb16af098ffc6f6d9d72e92ac50448a591c88
gayu3-art <gayathrip1808@gmail.com>
2026-01-15 13:40:08 +0530

    add search and filter

--- students/views.py
+++ students/views.py
@@ -2,6 +2,7 @@ from django.views.generic import ListView, DetailView, CreateView, UpdateView, D
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from django.contrib.messages.views import SuccessMessageMixin # Optional but nice
+from django.db.models import Q
 from .models import Student
 from .forms import StudentForm
 
@@ -10,6 +11,23 @@ class StudentListView(LoginRequiredMixin, ListView):
     template_name = 'students/student_list.html'
     context_object_name = 'students'
     ordering = ['-date_of_joining']
+    paginate_by = 10
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        query = self.request.GET.get('q')
+        if query:
+            queryset = queryset.filter(
+                Q(name__icontains=query) |
+                Q(email__icontains=query) |
+                Q(course__icontains=query)
+            )
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['query'] = self.request.GET.get('q', '')
+        return context
 
 class StudentDetailView(LoginRequiredMixin, DetailView):
     model = Student
